64f02f1e22e838d8e41d008cfda14e0dccbff7ac
Dennis Angermann <dennis.angermann@tn.techstarter.de>
2025-01-08 16:23:04 +0100

    Update Hausaufgabe08012025.py

--- Hausaufgabe08012025.py
+++ Hausaufgabe08012025.py
@@ -1,19 +1,34 @@
-class Pet:
-    def __init__(self, name, species, age, favorite_food, energy_level):
+# class Pet:
+# def __init__(self, name, species, age, favorite_food, energy_level):
+# self.name = name
+# self.species = species
+# self.age = age
+# self.favorite_food = favorite_food
+# self.energy_level = energy_level
+
+# def get_description(self):
+# return f"{self.name} + {self.species} + {self.age}
+
+# def play(self, duration):
+
+# def feed(self, food):
+
+
+# my_pet1 = Pet("Bello", "Hund", 5, "Knochen", 100)
+# my_pet2 = Pet("Mimi", "Katze", 3, "Fisch", 100)
+# print(f"{my_pet1}")
+# print(f" Mein {self.species} {self.name} ist {self.age} Jahre alt.")
+
+
+class Haustier:
+    def __init__(self, name, species, age):
         self.name = name
         self.species = species
         self.age = age
-        self.favorite_food = favorite_food
-        self.energy_level = energy_level
 
     def get_description(self):
-        return self.name + self.species + self.age
-
-    # def play(self, duration):
-
-    # def feed(self, food):
+        print(f"Mein {self.species} {self.name} ist {self.age} Jahre alt.")
 
 
-my_pet1 = Pet("Bello", "Hund", 5, "Knochen", 100)
-my_pet2 = Pet("Mimi", "Katze", 3, "Fisch", 100)
-print(f" Mein {self.species} {self.name} ist {self.age} Jahre alt.")
+my_pet1 = ("Bello", "Hund", 5, "Knochen", 100)
+my_pet1.get_description
